cb89f24ec3e1bda446257f1212173ae80fa4aef5
Ana Karina Gonzalez <anakarina.dev@gmail.com>
2023-08-09 12:27:11 -0500

    add:tkinder bookstore

---
+++ table-expenses/table.py
@@ -0,0 +1,17 @@
+# activavion de nuestro entorno virtual: env\Scripts\activate.bat
+
+import tkinter as tk
+"""biblioteca de interfaz gráfica de usuario (GUI) que
+ forma parte de la librería estándar de Python. Permite 
+ crear ventanas, botones, campos de entrada, menús y otros
+componentes visuales en aplicaciones de escritorio de manera sencilla"""
+
+
+def main():  # definicion de una funcion
+    root = tk.Tk()
+
+    root.mainloop()# indocamos el final de la ejecucion de la aplicacion (de nuestro bucle)
+
+
+if __name__ == '__main__':
+    main()
