5bc3988adaa0f1fccfc212b56ad35df1aa1d7b33
Ana Karina Gonzalez <anakarina.dev@gmail.com>
2023-08-23 19:20:52 -0500

    add: creating save, cancel and new buttons

--- table-expenses/client/gui_app.py
+++ table-expenses/client/gui_app.py
@@ -53,13 +53,27 @@ class Frame(tk.Frame):# utilizo la clase Frame de la libreria tk y creo una subc
             self.entry_description.config(width=50,state='disabled', font=('Arial',12))
             self.entry_description.grid(row=3,column=1, padx=10, pady=10)
 
+            # Botones guardar , cancelar y nuevo 
+
+            self.button_new = tk.Button(self, text='Nuevo')
+            self.button_new.config(width=20, font=('Arial',12,'bold'), fg='#DAD5D6', bg= '#158645',cursor='hand2', activebackground='#35BD6F')
+            self.button_new.grid(row= 4, column=0, padx=10, pady=10 )
+
+            self.button_save = tk.Button(self, text='Guardar')
+            self.button_save.config(width=20, font=('Arial',12,'bold'), fg='#DAD5D6', bg= '#3586DF',cursor='hand2', activebackground='#3586DF')
+            self.button_save.grid(row= 4, column=1, padx=10, pady=10 )
+
+            self.button_cancel = tk.Button(self, text='Cancelar')
+            self.button_cancel.config(width=20, font=('Arial',12,'bold'), fg='#DAD5D6', bg= '#E15370',cursor='hand2', activebackground='#E15370')
+            self.button_cancel.grid(row= 4, column=2, padx=10, pady=10 )
+
             
     
     def __init__(self, root = None):
         super().__init__(root,width= 480, height=320 )
         self.root=root
         self.pack()
-        self.config(bg='blue') # configuramos el tamaño del contenedor que hemos creado en la variable frame
+        #self.config(bg='blue') # configuramos el tamaño del contenedor que hemos creado en la variable frame
         self.expense_data()
 
    
